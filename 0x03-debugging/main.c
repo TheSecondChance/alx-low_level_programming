f80a721af130dfdfa996cc420fc36fdeffdb9f73
eyasu <mesakmegerem@gmail.com>
2023-06-21 07:56:24 -0700

    complicated

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Debugging now.
+*
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
