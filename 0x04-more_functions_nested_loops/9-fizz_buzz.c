b57b4f21f4215d1eb959ae84f100bac3e15a5e34
eyasu <mesakmegerem@gmail.com>
2023-06-22 09:31:33 -0700

    change output

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,23 +9,25 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; num <= 100; num++)
+	num = 1;
+	printf("%d", num);
+	for (num = 2; num <= 100; num++)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (num % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else
 		{
-			printf("%d ", num);
+			printf(" %d", num);
 		}
 	}
 	printf("\n");
