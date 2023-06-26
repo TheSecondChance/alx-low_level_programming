e9c54bc146d21a2c57e74e18d3fe45b2f8d31254
eyasu <mesakmegerem@gmail.com>
2023-06-26 05:41:34 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - updates the value it points to 98.
+ *
+ * @n: parameter that change
+ * Return: always zero.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef Main_H
+#define Main_h
+
+_putchar.c
+void reset_to_98(int *n);
+#endif
