356c5501d24352f1cef203e7356bf59d0f4a8500
eyasu <mesakmegerem@gmail.com>
2023-06-29 13:53:23 -0700

    main.h add prototype

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,5 +4,8 @@
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *leet(char *s);
+char *cap_string(char *s);
+void print_number(int n);
+char _putchar(char);
 
 #endif
