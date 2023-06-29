4fea252f90076c1f313ac7430e71596ab8f3762b
eyasu <mesakmegerem@gmail.com>
2023-06-29 14:40:01 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
- * print_number - it print number that integer.
- * @n: the number that pass
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
 void print_number(int n)
 {
@@ -10,9 +12,9 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n >= 10)
-	{
+
+	if (n / 10 != 0)
 		print_number(n / 10);
-	}
-	_putchar((n % 10) + '0');
+
+	_putchar(n % 10 + '0');
 }
