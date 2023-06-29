f0ebd8d8b7e0ab2a06058670182a1586f9eeace3
eyasu <mesakmegerem@gmail.com>
2023-06-29 14:24:22 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,5 +7,7 @@ char *leet(char *s);
 char *cap_string(char *s);
 void print_number(int n);
 char _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
