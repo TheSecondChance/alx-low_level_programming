9cdeae6c681fa7dba0522eb68df7cd32ef0431c8
eyasu <mesakmegerem@gmail.com>
2023-07-03 15:03:01 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_diagsums - function that prints the sum of two diagonals.
+ * @a: first element of squar
+ * @size: the size of the square
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += * (a + i*size + i);
+		sum2 += * (a + i*size + size - 1 - i);
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
