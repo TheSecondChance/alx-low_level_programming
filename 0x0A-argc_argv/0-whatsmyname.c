ab304feca163c7c4e065c7caf40c46951a0d27b2
eyasu <mesakmegerem@gmail.com>
2023-07-07 11:39:46 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv: pointer array that contains the program.
  * Return: zero.
  */
-int main(int argc, __attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
