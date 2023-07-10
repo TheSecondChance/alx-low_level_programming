6e7980a8cc33fc2ba33181d9b7bc6d79d96dfd9e
eyasu <mesakmegerem@gmail.com>
2023-07-10 13:06:07 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,12 +16,13 @@ char *create_array(unsigned int size, char c)
 
 	ptr = (char *)malloc(size * sizeof(c));
 
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
 	}
 
-	if (ptr == NULL)
-		return (NULL);
 	return (ptr);
 }
