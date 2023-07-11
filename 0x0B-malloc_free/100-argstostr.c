3cd50f7f671be78bd6b3d5a8aa7e8a6ec2822a4c
eyasu <mesakmegerem@gmail.com>
2023-07-11 06:11:27 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,7 +13,7 @@ char *argstostr(int ac, char **av)
 	char *st;
 	int i, j, len = 0, pos = 0;
 
-	if (ac == 0; || av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
