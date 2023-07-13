731d4b3a00e0ec94d754f2a767beaf8d27ecbc35
eyasu <mesakmegerem@gmail.com>
2023-07-13 02:27:58 -0700

    creat cocate sring

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second strig
+ * @n: amonte of bytes.
+ * Return: conten s1 and n bytes of s2 and null termintor.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *concat;
+	int i, j, co1 = 0, co2 = 0;
+
+	if (s1 == NULL)
+		return (s1 = "");
+	if (s2 == NULL)
+		return (s2 = "");
+	while (s1[co1] != '\0')
+		co1++;
+	while (s2[co2] != '\0')
+		co2++;
+	concat = malloc((co1 + co2 + 1) * sizeof(char));
+	if (concat == NULL)
+	{
+		free(concat);
+		return (NULL);
+	}
+	for (i = 0; i < co1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j <= n; j++, i++)
+		concat[i] = s2[i];
+	return (concat);
+}
