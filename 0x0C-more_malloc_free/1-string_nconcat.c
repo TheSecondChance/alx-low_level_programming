5f74e91f5aa912d9a65718ac3f5fba2dbcd0e8f8
eyasu <mesakmegerem@gmail.com>
2023-07-13 03:38:34 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,32 +9,38 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int i, co1 = 0, co2 = 0;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
 	if (s1 == NULL)
-		return (s1 = "");
+		s1 = "";
+
 	if (s2 == NULL)
-		return (s2 = "");
+		s2 = "";
 
-	while (s1[co1] != '\0')
-		co1++;
-	while (s2[co2] != '\0')
-		co2++;
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-	if (n >= co2)
-		n = co2;
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
 
-	concat = malloc((co1 + n + 1) * sizeof(char);
+	if (n > ls2)
+		n = ls2;
 
-	if (concat == NULL)
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
 		return (NULL);
 
-	for (i = 0; i < two; i++)
-		if (i < co1)
-			concat[i] = s1[i];
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
 		else
-			concat[i] = s2[i - co1];
-	concat[i] = '\0';
-	return (concat);
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
