c7eb4295a348bff9eb4902b899ff015b34df6d65
eyasu <mesakmegerem@gmail.com>
2023-07-13 03:25:44 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int i, j, co1 = 0, co2 = 0, two;
+	unsigned int i, co1 = 0, co2 = 0, two;
 
 	if (s1 == NULL)
 		return (s1 = "");
