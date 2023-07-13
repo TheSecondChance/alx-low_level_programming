b5a4ec7e5f5443c6225331a84714adf2ed2314df
eyasu <mesakmegerem@gmail.com>
2023-07-13 02:49:11 -0700

    creae concate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,8 +34,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < two; i++)
 		if (i < co1)
 			concat[i] = s1[i];
-	for (j = 0; j <= n; j++, i++)
-		concat[i] = s2[j];
+		else
+		concat[i] = s2[i - co1];
 	concate[i] = '\0';
 	return (concat);
 }
