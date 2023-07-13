708e7f772cfabac02b8b95745aa9406dd98041f3
eyasu <mesakmegerem@gmail.com>
2023-07-13 02:14:44 -0700

    modify return

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "main.h"
 /**
  * malloc_checked - allocates memory using malloc.
@@ -11,4 +12,5 @@ void *malloc_checked(unsigned int b)
 	ptr = malloc(b);
 	if (ptr == NULL)
 		exit(98);
+	return (ptr);
 }
