f60f67853ea1366fcb2d2cd28ff90a45b348b349
eyasu <mesakmegerem@gmail.com>
2023-07-19 13:12:59 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - function searches for integer
+ * @array: array that searched.
+ * @size: size of array
+ * @cmp: pointer function.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		return (-1);
+		if (size <= 0)
+		return (-1);
+		for (i = 0; i < size; i++)
+			if (cop(array[i]))
+				return (i);
+	}
+	return (-1);
+}
