b0522a5214fb5482191fd1a2e22c8f64bbf8751f
eyasu <mesakmegerem@gmail.com>
2023-07-27 08:45:08 -0700

    create one

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,17 @@
+#include "list.h"
+/**
+ * print_list - print all the element of a lsit_t list.
+ * @h: header passed
+ * Return: number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	int count;
+
+	while (s != NULL)
+	{
+		h->next;
+		count++;
+	}
+	return (count);
+}
