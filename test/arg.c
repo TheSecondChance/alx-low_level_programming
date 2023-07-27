5d03004a7445d4934f978ceaceb08e98e66d8ce4
eyasu <mesakmegerem@gmail.com>
2023-07-27 08:32:32 -0700

    delet

---
+++ test/arg.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+  int count;
+
+  printf ("This program was called with \"%s\".\n",argv[0]);
+
+  if (argc > 1)
+    {
+      for (count = 1; count < argc; count++)
+	{
+		if (argv[1] == 'A')
+		{
+		      	printf("argv[%d] = %s\n", count, argv[count]);
+		}
+	}
+    }
+  else
+    {
+      printf("The command had no other arguments.\n");
+    }
+
+  return 0;
+}
