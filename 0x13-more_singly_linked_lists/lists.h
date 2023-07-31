d7c1584ef09842ec1177cf5e18cc19a26a43a2c5
eyasu <mesakmegerem@gmail.com>
2023-07-31 14:11:23 -0700

    dlet

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,7 +17,6 @@ typedef struct listint_s
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
