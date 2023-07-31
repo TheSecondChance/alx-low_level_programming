a2d3316f607b06b78effe071805eb3221917cc9f
eyasu <mesakmegerem@gmail.com>
2023-07-31 15:16:27 -0700

    3 int the end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * add_nodeint_end - add a new node at the end of lsit.
+ * @head: the fist node
+ * @n: new nodes
+ * Return: new element or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	if (n == NULL)
+		return (NULL);
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+	{
+		free(n);
+		return (NULL);
+	}
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+		*head = new;
+
+	else
+	{
+		lsitint_t *lastNode = *head;
+
+		while (lastNode->next != NULL)
+		{
+			lastNode = lastNode->next;
+		}
+		lastNode->next = new;
+	}
+
+}
