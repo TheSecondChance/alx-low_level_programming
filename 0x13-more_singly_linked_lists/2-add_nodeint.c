60b179b160e5efb14fa6198aef9056967513ee38
eyasu <mesakmegerem@gmail.com>
2023-07-31 14:58:24 -0700

    remeved function

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -18,18 +18,3 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	return (*head);
 }
-/**
- * print_listint - print the elemnet
- * @n: number of that print
- * Return: none
- */
-listint_t print_listint(listint_t *n)
-{
-	listint_t *temp = n;
-
-	while (temp != NULL)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
-	}
-}
