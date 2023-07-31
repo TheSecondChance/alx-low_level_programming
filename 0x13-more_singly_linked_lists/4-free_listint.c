deb4885c8f6cb9b780b3102db8608b08663000cd
eyasu <mesakmegerem@gmail.com>
2023-07-31 15:35:16 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - function that free a list.
+ * @head: that frees nodes
+ * Return: none it's void.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
