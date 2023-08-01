13d3daae86d2ea66fee5a5177d37b286663177ba
eyasu <mesakmegerem@gmail.com>
2023-08-01 14:30:34 -0700

    add node

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,6 +15,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
 #include <stdio.h>
 #include <stdlib.h>
 
