acee1754d8f8f71afc56b54e6a31bba4581d56aa
eyasu <mesakmegerem@gmail.com>
2023-08-01 13:26:38 -0700

    fix betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,41 +8,37 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-    listint_t *temp1, *temp2;
-    unsigned int i = 0;
+	listint_t *temp1, *temp2;
+	unsigned int i = 0;
 
-    if (head == NULL)
-        return NULL;
+	if (head == NULL)
+		return (NULL);
 
-    temp2 = malloc(sizeof(listint_t));
-    if (temp2 == NULL)
-        return NULL;
+	temp2 = malloc(sizeof(listint_t));
+	if (temp2 == NULL)
+		return (NULL);
+	temp2->n = n;
+	temp2->next = NULL;
+	if (idx == 0)
+	{
+		temp2->next = *head;
+		*head = temp2;
 
-    temp2->n = n;
-    temp2->next = NULL;
+		return (temp2);
+	}
+	temp1 = *head;
+	while (temp1 != NULL && i < idx - 1)
+	{
+		temp1 = temp1->next;
+		i++;
+	}
+	if (temp1 == NULL)
+	{
+		free(temp2);
+		return (NULL);
+	}
+	temp2->next = temp1->next;
+	temp1->next = temp2;
 
-    if (idx == 0)
-    {
-        temp2->next = *head;
-        *head = temp2;
-        return temp2;
-    }
-
-    temp1 = *head;
-    while (temp1 != NULL && i < idx - 1)
-    {
-        temp1 = temp1->next;
-        i++;
-    }
-
-    if (temp1 == NULL)
-    {
-        free(temp2);
-        return NULL;
-    }
-
-    temp2->next = temp1->next;
-    temp1->next = temp2;
-
-    return temp2;
+	return (temp2);
 }
