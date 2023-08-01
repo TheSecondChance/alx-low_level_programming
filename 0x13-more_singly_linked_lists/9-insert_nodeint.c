e382f478f75e5163507ebc631cd212ab8b82012e
eyasu <mesakmegerem@gmail.com>
2023-08-01 13:05:42 -0700

    word error fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,9 +8,8 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
 	listint_t *temp1 = *head;
-	lsitint_t *temp2 = malloc(sizeof(listint_t));
+	listint_t *temp2 = malloc(sizeof(listint_t));
 
 	temp2->n = n;
 	temp2->next = NULL;
