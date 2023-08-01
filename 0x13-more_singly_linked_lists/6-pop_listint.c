e127dce007b380952ef87562171c812e355c5e43
eyasu <mesakmegerem@gmail.com>
2023-08-01 04:45:21 -0700

    some change

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -6,11 +6,13 @@
  */
 int pop_listint(listint_t **head)
 {
+	int n;
+
 	if (*head == NULL)
-		return (NULL);
-	if (*head->next = NULL)
+		return (0);
+
+	if ((*head)->next == NULL)
 	{
-		free(*head);
 		*head = NULL;
 	}
 	else
@@ -18,14 +20,15 @@ int pop_listint(listint_t **head)
 		listint_t *temp = *head;
 		listint_t *temp2 = *head;
 
-		while (temp->link != NULL)
+		while (temp->next != NULL)
 		{
+			n = temp->n;
 			temp2 = temp;
-			temp = temp->link;
+			temp = temp->next;
 		}
-		temp2->link = NULL;
+		temp2->next = NULL;
 		free(temp);
 		temp = NULL;
 	}
-	return (*head);
+	return (n);
 }
