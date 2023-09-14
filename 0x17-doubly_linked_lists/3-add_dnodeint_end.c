97d871bf38e64722a6f7cff0d1f1784d3bcff29c
eyasu <mesakmegerem@gmail.com>
2023-09-14 09:42:43 -0700

    3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -8,7 +8,7 @@
  **/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new, *find_last = *head;
+	dlistint_t *new_node, *find_last = *head;
 
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
