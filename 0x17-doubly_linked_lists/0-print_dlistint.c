2a9bb0a0dd693a9a2427ae3fb9ac05959f643f97
eyasu <mesakmegerem@gmail.com>
2023-09-14 07:31:17 -0700

    first

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,20 @@
+#include <lists.h>
+/**
+ * print_dlistint - print the elements of a dlistint_t
+ * @h: pointer of the list
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *temp = h;
+	size_t count = 0;
+
+	while (temp != NULL)
+	{
+		printf("%i\n", node->n);
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS
+#define LISTS
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+size_t print_dlistint(const dlistint_t *h);
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Alx project
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
